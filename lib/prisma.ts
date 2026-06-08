65a4fa8f7cf4b34877582652e184f1e77f68310c
Krisnanda <baguskrisna424@gmail.com>
2026-06-08 11:38:56 +0800

    feat: added some folder

---
+++ lib/prisma.ts
@@ -0,0 +1,20 @@
+cat > lib/prisma.ts << 'EOF'
+import { PrismaClient } from "@/app/generated/prisma";
+import { EOF } from "dns/promises";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+EOF
