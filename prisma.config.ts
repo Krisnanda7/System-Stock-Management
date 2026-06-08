dcdfab272704fd1196eef672b55f1b93b032bbd3
Krisnanda <baguskrisna424@gmail.com>
2026-06-08 11:05:36 +0800

    fix: connection in prisma connection for supabase

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: "postgresql://postgres.iisslbxxqdwwdcfwunrz:StokApp2024secure@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres",
   },
 });
