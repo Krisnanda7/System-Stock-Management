4be18be5b35d17614a396fc0dfb96aa6d3e218e8
Krisnanda <baguskrisna424@gmail.com>
2026-06-09 15:36:41 +0800

    chore: delete some file

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <link rel="icon" href="/chart.png" />
-      <link rel="shortcut icon" href="/chart.png" />
-      <link rel="apple-touch-icon" href="/chart.png" />
-    </>
-  );
-}
