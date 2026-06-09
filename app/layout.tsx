82a7ce5fe7f8ba49161849349268708f18884104
Krisnanda <baguskrisna424@gmail.com>
2026-06-09 14:17:36 +0800

    feat: modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "StokApp — Manajemen Stok",
   description: "Sistem manajemen stok barang",
   icons: {
-    icon: "/icon.png"
+    icon: "/chart.svg",
   },
 };
 
